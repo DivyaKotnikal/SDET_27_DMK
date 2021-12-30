fe0c716b1342c540d6f88c353f8f1ab1b76cf9b9
Divya <Divya@DESKTOP-NP0OUFO>
2021-12-30 15:20:50 +0530

    ITS a first git folder

---
+++ com.crm.Demo/src/test/java/com/crm/Demo/Contacts/ContactTest.java
@@ -0,0 +1,17 @@
+package com.crm.Demo.Contacts;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContact()
+	{
+		System.out.println("creating contact");
+	}
+	@Test
+	public void deleteContact()
+	{
+		System.out.println("deleting contact");
+	}
+}
---
+++ com.crm.Demo/src/test/java/com/crm/Demo/Orgnization/orgTest.java
@@ -0,0 +1,18 @@
+package com.crm.Demo.Orgnization;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	@Test
+	public void createOrg()
+	{
+		System.out.println("Creating org");
+	}
+	
+	@Test
+	public void deleteOrg()
+	{
+		System.out.println("delete org");
+	}
+
+}
